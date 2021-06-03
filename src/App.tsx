b7ed2ca6cac1a654e7ded75772d5bc69db3bbaaf
Olatade <4tade01@gmail.com>
2021-06-03 04:55:09 +0100

    Add Tailwind and formik

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/img/fliqpay-logo.png';
+import './styles/App.css';
+import FormPayoutAmount from './components/form-payout-amount';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <header className="nav">
+        <img src={logo} className="nav__logo" alt="logo" />
+        <div className="progress-bar">This is the progress bar</div>
       </header>
+
+      <div className="body">
+        <FormPayoutAmount/>
+      </div>
+
     </div>
   );
 }
