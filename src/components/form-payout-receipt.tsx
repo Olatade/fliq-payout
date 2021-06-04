e043ab881b248b70129db130ca96bdc92b49f4c3
Olatade <4tade01@gmail.com>
2021-06-04 01:31:30 +0100

    Rename input fields in receipt form

--- src/components/form-payout-receipt.tsx
+++ src/components/form-payout-receipt.tsx
@@ -1,37 +1,45 @@
 import '../styles/FormPayoutReceipt.css';
 import React from 'react';
 import '../styles/App.css';
- import { Formik, Form, Field, ErrorMessage } from 'formik';
- import * as Yup from "yup";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from "yup";
 import 'alpinejs';
-import {useState} from 'react'; 
+import { useState } from 'react';
 
 
 
-function FormPayoutReceipt () {
+function FormPayoutReceipt() {
 
+  // set hook to toggle tab state when tab is clicked
   const [toggleState, setToggleState] = useState(1);
 
   const toggleTab = (index: any) => {
-    console.log(index);
+    // set the toggleState to the index of the selected tab
     setToggleState(index);
   };
 
   return (
     <Formik
-      initialValues={{ youSend: "", recipientGets: ""}}
+      initialValues={{ email: "", fullName: "", iban: "", swift: "" }}
+      
+      // validation configuration using Yup
       validationSchema={Yup.object({
-        youSend: Yup.string()
-        .max(15, "Must be 15 characters or less")
-        .required(`Required`),
-        recipientGets: Yup.string()
-        .max(15, "Must be 15 characters or less")
-        .required("Required"),
+        email: Yup.string()
+          .max(15, "Must be 15 characters or less"),
+
+        fullName: Yup.string()
+          .max(15, "Must be 15 characters or less")
+          .required("Required"),
+        iban: Yup.string()
+          .max(15, "Must be 15 characters or less")
+          .required("Required"),
+        swift: Yup.string()
+          .max(15, "Must be 15 characters or less")
+          .required("Required"),
       })}
       onSubmit={(values, { setSubmitting }) => {
         setTimeout(() => {
           alert(JSON.stringify(values, null, 2));
-
           setSubmitting(false);
         }, 400);
       }}
@@ -42,29 +50,29 @@ function FormPayoutReceipt () {
           <p className="form-head__sub">Who are you sending money to?</p>
         </div>
 
-        {/* You send */}
+        {/* Email */}
         <div>
           <div className="pay-input">
-            <label className="pay-input__label"  htmlFor="youSend">You send</label>
-            <Field className="pay-input__price" name="youSend" type="text" placeholder="0.00" />
+            <label className="pay-input__label" htmlFor="email">Their email (optional)</label>
+            <Field className="pay-input__price" name="email" type="text" placeholder="" />
           </div>
-          <ErrorMessage render={msg => <div className="pay-input__error-message">{msg}</div>} name="youSend" />
+          <ErrorMessage render={msg => <div className="pay-input__error-message">{msg}</div>} name="email" />
         </div>
 
-        {/* Recepient gets */}
+        {/* Full name */}
         <div>
           <div className="pay-input">
-            <label className="pay-input__label"  htmlFor="recipientGets">Recipient gets </label>
-            <Field className="pay-input__price" name="recipientGets" type="text" placeholder="0.00" />
+            <label className="pay-input__label" htmlFor="fullName">Full name of the account holder</label>
+            <Field className="pay-input__price" name="fullName" type="text" placeholder="" />
           </div>
-          <ErrorMessage render={msg => <div className="pay-input__error-message">{msg}</div>} name="recipientGets" />
+          <ErrorMessage render={msg => <div className="pay-input__error-message">{msg}</div>} name="fullName" />
         </div>
 
         <div className="form-subheading ">
           <p className="form-subheading__main">Bank details</p>
         </div>
 
-        
+
         <div className="tab-container">
           <div className="tab-container__buttons">
             <button className={toggleState === 1 ? "tab-button active-tab-button" : "tab-button"} onClick={() => toggleTab(1)}>
@@ -83,49 +91,49 @@ function FormPayoutReceipt () {
               {/* IBAN */}
               <div>
                 <div className="pay-input">
-                  <label className="pay-input__label" htmlFor="youSend">IBAN</label>
-                  <Field className="pay-input__price" name="youSend" type="text" placeholder="0.00" />
+                  <label className="pay-input__label" htmlFor="iban">IBAN</label>
+                  <Field className="pay-input__price" name="iban" type="text" placeholder="0.00" />
                 </div>
-                <ErrorMessage render={msg => <div className="pay-input__error-message">{msg}</div>} name="youSend" />
+                <ErrorMessage render={msg => <div className="pay-input__error-message">{msg}</div>} name="iban" />
               </div>
             </div>
 
-          <div className={toggleState === 2 ? "tab-content  active-tab-content" : "tab-content"}>
+            <div className={toggleState === 2 ? "tab-content  active-tab-content" : "tab-content"}>
 
-              {/* You send */}
+              {/* SWIFT */}
               <div>
                 <div className="pay-input">
-                  <label className="pay-input__label" htmlFor="youSend">SWIFT / BIC code</label>
-                  <Field className="pay-input__price" name="youSend" type="text" placeholder="BUKBGB22" />
+                  <label className="pay-input__label" htmlFor="swift">SWIFT / BIC code</label>
+                  <Field className="pay-input__price" name="swift" type="text" placeholder="BUKBGB22" />
                 </div>
-                <ErrorMessage render={msg => <div className="pay-input__error-message">{msg}</div>} name="youSend" />
+                <ErrorMessage render={msg => <div className="pay-input__error-message">{msg}</div>} name="swift" />
               </div>
 
-              {/* Recepient gets */}
+              {/* IBAN EUROPE */}
               <div>
                 <div className="pay-input">
-                  <label className="pay-input__label"  htmlFor="recipientGets">IBAN / Account Number</label>
-                  <Field className="pay-input__price" name="recipientGets" type="text" placeholder="01234567891" />
+                  <label className="pay-input__label" htmlFor="iban">IBAN</label>
+                  <Field className="pay-input__price" name="iban" type="text" placeholder="0.00" />
                 </div>
-                <ErrorMessage render={msg => <div className="pay-input__error-message">{msg}</div>} name="recipientGets" />
+                <ErrorMessage render={msg => <div className="pay-input__error-message">{msg}</div>} name="iban" />
               </div>
-            
+
+            </div>
           </div>
-      </div>
         </div>
-  
+
 
 
         <div className="pay-input__buttons">
           <a className="pay-input__buttons--compare" href="/">Compare Rates</a>
           <button className="pay-input__buttons--continue" type="submit">Continue</button>
         </div>
-        
+
       </Form>
     </Formik>
   );
 }
 
- 
 
- export default FormPayoutReceipt;
+
+export default FormPayoutReceipt;
